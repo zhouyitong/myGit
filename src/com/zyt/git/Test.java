37e2bf694a18803dc527f5af8f4af3617c17c5f5
zyt <593218966@qq.com>
2016-01-05 19:55:13 +0800

    jhgfds

---
+++ src/com/zyt/git/Test.java
@@ -0,0 +1,11 @@
+package com.zyt.git;
+
+/**
+ * Created by Administrator on 2016/1/5.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hi Git");
+    }
+}
